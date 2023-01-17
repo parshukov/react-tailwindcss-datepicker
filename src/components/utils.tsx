aba664a43535b9ac4a271e91916a150295758d48
Nikolay Parshukov <parshukov.nikolay@gmail.com>
2023-01-17 08:35:21 +0300

    Update utils.tsx

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -44,7 +44,7 @@ export const CloseIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => {
             strokeWidth={1.5}
             stroke="currentColor"
         >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L10 6M6 6l12 12" />
         </svg>
     );
 };
